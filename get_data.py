adefa9e667f4c9813cfebfd77bef16649f11a5fb
sweeney <sweeneylee@qq.com>
2022-06-20 13:41:11 +0800

    add exception catch

--- get_data.py
+++ get_data.py
@@ -106,8 +106,9 @@ def save_ards_data(base_ards_data, thread_number=0):
     ards_data.to_csv(os.path.join(output_data_path,
                                   data_name + '_%d.csv' % thread_number), index=False)
 
-    print('Error list:\n', error_index_list)
-    pd.DataFrame(error_index_list, columns=['icu_stay_id']).to_csv('error_%d.csv' % thread_number, index=False)
+    if len(error_index_list) > 0:
+        print('Error list:\n', error_index_list)
+        pd.DataFrame(error_index_list, columns=['icu_stay_id']).to_csv('error_%d.csv' % thread_number, index=False)
 
     sql_connector.close()
 
